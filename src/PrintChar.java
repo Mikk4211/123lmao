fbd5adc3133103bf3c5485f2afae0e72ef8e8f92
Mikkel Sørensen <mik4211@edu.easj.dk>
2018-04-25 09:05:53 +0200

    Initial commit

---
+++ src/PrintChar.java
@@ -0,0 +1,16 @@
+public class PrintChar implements Runnable {
+    private char charToPrint;
+    private int times;
+
+    public PrintChar(char c, int t) {
+        this.charToPrint = c;
+        this.times = t;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < times; i++){
+            System.out.println(charToPrint);
+        }
+    }
+}
